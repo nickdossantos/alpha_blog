13453cd4cc8f69468998ab2142cce43e4b45b0a2
nickdossantos <nickdos32@gmail.com>
2018-06-05 11:00:32 -0400

    basic CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -14,8 +22,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Your article was saved"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Your article was deleted"
   end
 
   private
